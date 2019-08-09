5aec3d09c05189f94e774bd953fc943c773eb217
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 20:07:49 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -9,16 +9,16 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-          <ReviewInput
-            addReview={this.props.addReview}
-            restaurantId={this.props.restaurant.id}
-          />
-          <Reviews
-            reviews={this.props.reviews}
-            restaurantId={this.props.restaurant.id}
-            deleteReview={this.props.deleteReview}
-          />
-        </div>
+        <ReviewInput
+          addReview={this.props.addReview}
+          restaurantId={this.props.restaurant.id}
+        />
+        <Reviews
+          reviews={this.props.reviews}
+          restaurantId={this.props.restaurant.id}
+          deleteReview={this.props.deleteReview}
+        />
+      </div>
     )
   }
 }
