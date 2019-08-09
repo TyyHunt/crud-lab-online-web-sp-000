e123fc9308beb5733a56826d4c00e98b928e8ec3
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 20:03:59 +0000

    Automatically backed up by Learn

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 class Review extends Component {
 
+  handleOnClick = () => {
+    this.props.deleteReview(this.props.review.id)
+  }
+
   render() {
-    const { review } = this.props
 
     return (
       <div>
